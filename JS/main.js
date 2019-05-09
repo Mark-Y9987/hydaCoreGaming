640b17cb1048eee00fbc00c55570ca5d3517edee
Mark Young <35600614+Mark-Y9987@users.noreply.github.com>
2019-05-09 08:15:06 -0400

    Store update

--- JS/main.js
+++ JS/main.js
@@ -1,38 +1,38 @@
 var slideout = new Slideout({
-    'panel': document.getElementById('panel'),
-    'menu': document.getElementById('menu'),
-    'padding': 256,
-    'tolerance': 70
+	panel: document.getElementById('panel'),
+	menu: document.getElementById('menu'),
+	padding: 256,
+	tolerance: 70
 });
 
 // Toggle button
 document.querySelector('.toggle-button').addEventListener('click', function () {
-    slideout.toggle();
+	slideout.toggle();
 });
 
 function close(eve) {
-    eve.preventDefault();
-    slideout.close();
+	eve.preventDefault();
+	slideout.close();
 }
 
 slideout
-    .on('beforeopen', function () {
-        this.panel.classList.add('panel-open');
-    })
-    .on('open', function () {
-        this.panel.addEventListener('click', close);
-    })
-    .on('beforeclose', function () {
-        this.panel.classList.remove('panel-open');
-        this.panel.removeEventListener('click', close);
-    });
+	.on('beforeopen', function () {
+		this.panel.classList.add('panel-open');
+	})
+	.on('open', function () {
+		this.panel.addEventListener('click', close);
+	})
+	.on('beforeclose', function () {
+		this.panel.classList.remove('panel-open');
+		this.panel.removeEventListener('click', close);
+	});
 
 $(function () {
-    $(".rslides").responsiveSlides({
-        auto: true,
-        speed: 500,
-        timeout: 7000,
-        nav: false,
-        pause: true,
-    });
+	$('.rslides').responsiveSlides({
+		auto: true,
+		speed: 500,
+		timeout: 10000,
+		nav: false,
+		pause: true
+	});
 });
